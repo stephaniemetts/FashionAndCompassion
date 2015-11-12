6455f78e304f9c6fc9b7155d5ebe9b95e030936d
Sam Painter <sam.foy.painter@gmail.com>
2015-11-11 21:01:41 -0500

    Created lib class to seed database from shopify. We have real data now. Still need to handle nil skus

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,46 +8,5 @@
 
 Product.destroy_all
 
-Product.create!(title: "Necklace Half Moon", sku: "ABCDEGF", selling: true, notes: "A simple half moon necklace", color: "Silver",
-                collection: "Necklaces", warehouse_inventory: 8, cost_cents: 1770, retail_cents: 2999, wholesale_cents:2499)
-Product.create!(title: "Necklace Half Moon", sku: "ABCDEGD", selling: true, notes: "A simple half moon necklace", color: "Gold",
-                collection: "Necklaces", warehouse_inventory: 17, cost_cents: 1770, retail_cents: 2999, wholesale_cents:2499)
-Product.create!(title: "Necklace Half Moon", sku: "ABCDEGA", selling: true, notes: "A simple half moon necklace", color: "White Gold",
-                collection: "Necklaces", warehouse_inventory: 12, cost_cents: 1770, retail_cents: 2999, wholesale_cents:2499)
-Product.create!(title: "Bracelet Half Moon", sku: "GDENFGS", selling: false, notes: "A simple half moon bracelet", color: "Gold",
-                collection: "Bracelets", cotswold_inventory: 9, cost_cents: 2824, wholesale_cents: 4499, retail_cents: 4999,
-                warehouse_inventory: 7)
-Product.create!(title: "Bracelet Half Moon", sku: "GDENFGR", selling: true, notes: "A simple half moon bracelet", color: "Silver",
-                collection: "Bracelets", cotswold_inventory: 0, cost_cents: 2824, wholesale_cents: 4499, retail_cents: 4999,
-                warehouse_inventory: 18)
-Product.create!(title: "Bracelet Half Moon", sku: "GDENFGB", selling: false, notes: "A simple half moon bracelet", color: "Brass",
-                collection: "Bracelets", cotswold_inventory: 0, cost_cents: 2824, wholesale_cents: 4499, retail_cents: 4999,
-                warehouse_inventory: 4, blacklion_inventory: 8)
-Product.create!(title: "F&C T-Shirt", sku: "HSDF782", selling: true, notes: "Fashion and Compassion Logo T-Shirt", color: "White",
-                collection: "Shirts", cotswold_inventory: 0, cost_cents: 1249, wholesale_cents: 1999, retail_cents: 2599,
-                warehouse_inventory: 19)
-Product.create!(title: "F&C T-Shirt", sku: "HSDF784", selling: true, notes: "Fashion and Compassion Logo T-Shirt", color: "Black",
-                collection: "Shirts", cotswold_inventory: 0, cost_cents: 1249, wholesale_cents: 1999, retail_cents: 2599,
-                warehouse_inventory: 3, blacklion_inventory: 13)
-Product.create!(title: "F&C T-Shirt", sku: "HSDF783", selling: true, notes: "Fashion and Compassion Logo T-Shirt", color: "Blue",
-                collection: "Shirts", cotswold_inventory: 0, cost_cents: 1249, wholesale_cents: 1999, retail_cents: 2599,
-                warehouse_inventory: 31)
-Product.create!(title: "F&C T-Shirt", sku: "HSDF785", selling: true, notes: "Fashion and Compassion Logo T-Shirt", color: "Green",
-                collection: "Shirts", cotswold_inventory: 0, cost_cents: 1249, wholesale_cents: 1999, retail_cents: 2599,
-                warehouse_inventory: 0, bdbox_inventory: 6)
-Product.create!(title: "Ballot Skirt", sku: "YSHZ76F", selling: true, notes: "Basic Ballot Skirt", color: "Red",
-                collection: "Skirts", warehouse_inventory: 7, cost_cents:1879, wholesale_cents: 0, retail_cents: 2599)
-Product.create!(title: "Ballot Skirt", sku: "YSHZ76H", selling: true, notes: "Basic Ballot Skirt", color: "Burgandy",
-                collection: "Skirts", warehouse_inventory: 9, cost_cents:1879, wholesale_cents: 0, retail_cents: 2599)
-Product.create!(title: "Ballot Skirt", sku: "YSHZ76I", selling: true, notes: "Basic Ballot Skirt", color: "Seafoam",
-                collection: "Skirts", warehouse_inventory: 4, cost_cents:1879, wholesale_cents: 0, retail_cents: 2599)
-Product.create!(title: "Ballot Skirt", sku: "YSHZ76G", selling: true, notes: "Basic Ballot Skirt", color: "Peach",
-                collection: "Skirts", warehouse_inventory: 11, cost_cents:1879, wholesale_cents: 0, retail_cents: 2599)
-Product.create!(title: "Necklace Twisted Heart", sku: "A78BYS8", selling: true, notes: "Twisted Heart Necklace", color: "Silver",
-                collection: "Necklaces", warehouse_inventory: 5, cost_cents: 1889, retail_cents:2999, wholesale_cents:2499)
-Product.create!(title: "Necklace Twisted Heart", sku: "A78BYS9", selling: true, notes: "Twisted Heart Necklace", color: "Brass",
-                collection: "Necklaces", warehouse_inventory: 0, cost_cents: 1889, retail_cents:2999, wholesale_cents:2499)
-Product.create!(title: "Necklace Twisted Heart", sku: "A78BYS0", selling: true, notes: "Twisted Heart Necklace", color: "White Gold",
-                collection: "Necklaces", warehouse_inventory: 1, cost_cents: 1889, retail_cents:2999, wholesale_cents:2499)
-Product.create!(title: "Necklace Twisted Heart", sku: "A78BYS3", selling: true, notes: "Twisted Heart Necklace", color: "Stainless Steel",
-                collection: "Necklaces", warehouse_inventory: 8, bdbox_inventory: 7, cost_cents: 1889, retail_cents:2999, wholesale_cents:2499)
+ShopifyDataPull::ShopifyDataPull.populate
+ShopifyDataPull::ShopifyDataPull.wholesales
---
+++ lib/tasks/shopify_data_pull.rb
@@ -0,0 +1,45 @@
+module ShopifyDataPull
+  class ShopifyDataPull
+
+    def self.populate
+      api_key = ENV["SHOPIFY_API_KEY"]
+      password = ENV["SHOPIFY_PASSWORD"]
+      shop_url = "https://#{api_key}:#{password}@fashion-compassion.myshopify.com/admin"
+      ShopifyAPI::Base.site = shop_url
+      @sproducts = ShopifyAPI::Product.all
+      @sproducts.each do |sp|
+        sp.variants.each do |variant|
+          next if variant.sku == "" or variant.title.include?("wholesale")
+          product = Product.new
+          product.title = sp.title
+          product.sku = variant.sku
+          product.color = variant.title
+          product.collection = sp.vendor
+          product.warehouse_inventory = variant.inventory_quantity
+          product.cost = 0
+          product.wholesale = 0
+          product.retail = variant.price
+          product.selling = variant.inventory_policy == "continue"
+          product.save
+        end
+      end
+    end
+
+    def self.wholesales
+      api_key = ENV["SHOPIFY_API_KEY"]
+      password = ENV["SHOPIFY_PASSWORD"]
+      shop_url = "https://#{api_key}:#{password}@fashion-compassion.myshopify.com/admin"
+      ShopifyAPI::Base.site = shop_url
+      @sproducts = ShopifyAPI::Product.all
+      @sproducts.each do |sp|
+        sp.variants.each do |variant|
+          if variant.title.include?("wholesale")
+            product = Product.search(variant.sku,:sku).first
+            product.wholesale = variant.price
+            product.save
+          end
+        end
+      end
+    end
+  end
+end
